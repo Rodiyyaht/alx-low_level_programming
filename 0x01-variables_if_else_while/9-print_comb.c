b7bc0d3e43a766a7dd4893c3da01f0a077bc5844
Rodiyyaht <rodiekola@gmail.com>
2022-06-03 11:15:37 -0700

    Printing numbers with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+
+/**
+ * main - prints all possible combination of single digit numbers
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
