0e3da30597a92ad99f9cd650694fa4e6efd3a988
Rodiyyaht <rodiekola@gmail.com>
2022-06-07 14:38:57 -0700

    Printing time integer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,19 +11,18 @@ void jack_bauer(void)
 	i = 0;
 
 	while (i < 24)
-		j = 0;
 	{
-
-	    while (j < 60)
-	    {
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
-		_putchar((j / 10) + '0');
-		_putchar((j % 10) + '0');
-		_putchar('\n');
-		j++;
-	    }
-	    i++;
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
 	}
 }
