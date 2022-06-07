07f88a8de0e01c495c939b4e111feb146369f9e0
Rodiyyaht <rodiekola@gmail.com>
2022-06-07 16:27:41 -0700

    printing numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,19 +12,19 @@ int main(void)
 	j = 1;
 
 	k = 2;
+
 	for (i = 1; i <= 50; ++i)
 	{
 		if (j != 20365011074)
 		{
 			printf("%ld, ", j);
+		} else 
+		{
+			printf("%ld\n", j);
 		}
-		 else 
-		 {
-			 printf("%ld\n", j);
-		 }
-		 next = j + k;
-		 j = k;
-		 k = next;
+		next = j + k;
+		j = k;
+		k = next;
 	}
 	return (0);
 }
