97b937d23b0deb2922d1c986acd51886efabb7e7
Rodiyyaht <rodiekola@gmail.com>
2022-06-20 06:25:47 -0700

    Arrays and pointers are interesting

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,11 @@
 
 /**
  * *_memcpy - copies memory area
- * @dest: destination string
- * @src: source string
- * @n: max bytes to use
- * Return (dest)
+ * @dest: memory area
+ * @src: source
+ * @n: length of src
+ *
+ * Return: pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
