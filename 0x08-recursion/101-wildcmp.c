bb005c771905352d8c8db7d2aa119a0df25eb3ee
Rodiyyaht <rodiekola@gmail.com>
2022-06-22 06:14:10 -0700

    Wilcards are tricky

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,7 +7,7 @@ int wildcmp(char *s1, char *s2);
 
 /***
  * strlen_no_wilds - Returns the length of a string,
- *                   ignoring wildcard characters
+ * ignoring wildcard characters.
  * @str: The string to be measured
  * Return: The length
  */
