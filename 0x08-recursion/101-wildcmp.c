862fa3739e5bfda77e43561573e56f6095867cf4
Rodiyyaht <rodiekola@gmail.com>
2022-06-22 06:17:54 -0700

    Wildcard

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,10 +5,11 @@ void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
 
-/***
+/**
  * strlen_no_wilds - Returns the length of a string,
- * ignoring wildcard characters.
+ * ignoring wildcard characters
  * @str: The string to be measured
+ *
  * Return: The length
  */
 int strlen_no_wilds(char *str)
