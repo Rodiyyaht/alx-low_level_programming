c390eabdbcda7c3dc2f6025f2f000af06cbb1199
Rodiyyaht <rodiekola@gmail.com>
2022-06-22 03:13:15 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: the string to be measured
+ *
+ * Return : the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+	}
+
+	return (i);
+}
+
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,5 @@
 Task 0: A function that prints a string.
 
 Task 1: A function that prints a string in reverse.
+
+Task 2: A function that returns the length of a string.
