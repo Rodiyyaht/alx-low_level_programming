2afae8e52509375f560e5faceb59d760870acc34
Rodiyyaht <rodiekola@gmail.com>
2022-06-28 03:30:37 -0700

    Concatenating twoD arrays

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - concatenates all the arguments of your program
+ * each argument should be followed by a \n in the new string
+ * @ac: number of arguments
+ * @av: array of arguments
+ *
+ * Return: NULL if ac = 0 and av = NULL, or if the program fails
+ * else, a pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+
+	int arg, bit, fir, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (bit = 0; av[arg][bit]; bit++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) *size + 1);
+
+	if (str == NULL)	
+		return (NULL);
+	fir = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (bit = 0; av[arg][bit]; bit++)
+			str[fir++] = av[arg][bit];
+		str[fir++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -7,3 +7,7 @@ Task 2: It is a function that concatenates two strings.
 Task 3: It is a function that returns a pointer to a 2 dimensional array of integers.
 
 Task 4: It is a function that frees a 2 dimensional grid previously created by your alloc_grid function.
+
+Task 5: It is a function that concatenates all the arguments of your program.
+
+Task 6: It is a function that splits a string into words.
