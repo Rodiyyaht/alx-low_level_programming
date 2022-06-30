95458c4696540c73fe43ac09b7152a89f33f4cc9
Rodiyyaht <rodiekola@gmail.com>
2022-06-30 03:05:06 -0700

    More malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: If new_size == old_size - ptr
+ * if new_size == 0 and ptr is not NULL - NULL
+ * pointer to the newly allocated memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	ptr_copy = ptr;
+        mem = malloc(sizeof(*ptr_copy) * new_size);
+
+	if (mem == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	filler = mem;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
+	free(ptr);
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,11 @@
+Task 0: It is a function that allocates memory using malloc.
+
+Task 1: It is a function that concatenates two strings.
+
+Task 2: Itis a function that allocates memory for an array, using malloc.
+
+Task 3:It is a function that creates an array of integers.
+
+Task 100: It is a function that reallocates a memory block using malloc and free.
+
+Task 101: It is a program that multiplies two positive numbers.
