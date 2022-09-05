aa545f820bcce6e464b1d788f280a485b5de7bd5
Rodiyyaht <rodiekola@gmail.com>
2022-09-05 05:08:01 -0700

    Change

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Rodiyyaht/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -O /tmp/win.so https://github.com/Rodiyyaht/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
