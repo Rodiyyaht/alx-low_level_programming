0c2984462397efa9818a50afbc2a5690478288ae
Rodiyyaht <105075429+Rodiyyaht@users.noreply.github.com>
2022-09-06 23:48:18 +0100

    Update 101-make_me_win.sh
    Changing

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Rodiyyaht/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp/ https://raw.github.com/Rodiyyaht/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
