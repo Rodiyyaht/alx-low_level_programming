c4395587bb52c4a2e92375ff9b481f55595b128f
Rodiyyaht <rodiekola@gmail.com>
2022-09-06 23:36:25 +0100

    Adding make wish file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Rodiyyaht/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://raw.github.com/Rodiyyaht/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
